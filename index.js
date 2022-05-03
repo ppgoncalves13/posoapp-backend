92b0af89a2381ef358d7b3c89066b5a9fbad8e16
ppgoncalves13 <pp2004goncalves@gmail.com>
2022-05-03 18:38:53 +0100

    Added httpserver to the main in order to see if heruko doesnt stop the app from running

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const getNotifications = require("./functions/notifications")
 const schedule = require('node-schedule');
+var http = require('http');
+require('dotenv').config()
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end("<html></html>", 'utf-8');
+}).listen(process.env.PORT || 5000);
 
 // Repeat Queue every 2 mins
 console.log("Start notifications Queue...")
 
 const job = schedule.scheduleJob('*/1 * * * *', async () => await getNotifications());
+
